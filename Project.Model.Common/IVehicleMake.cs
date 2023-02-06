89911ceea9d0a9904f3b5a8b167e5eac391426f2
Jurica98 <jurematijanic98@gmail.com>
2023-02-06 13:52:58 +0100

    New Interface

---
+++ Project.Model.Common/IVehicleMake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model.Common
+{
+    public interface IVehicleMake
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string Abrv { get; set; }
+
+       
+    }
+}
---
+++ Project.Model.Common/IVehicleModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model.Common
+{
+    public interface IVehicleModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abrv { get; set; }
+
+        [ForeignKey("VehicleMake")]
+        public int VehicleMakeId { get; set; }
+
+        
+    }
+}
