6e33f3e889cfcf18e46ec9d59edf2c0d8d8db9d7
Jurica98 <jurematijanic98@gmail.com>
2023-02-06 13:48:25 +0100

    New migration

--- Project.DAL/Migrations/20230206121315_Inital_003.Designer.cs
+++ Project.DAL/Migrations/20230206121315_Inital_003.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project.DAL;
 
@@ -10,9 +11,11 @@ using Project.DAL;
 namespace Project.DAL.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230206121315_Inital_003")]
+    partial class Inital003
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
